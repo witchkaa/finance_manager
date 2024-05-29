14d6f8b32ae44ac13a7316db5179bc8d968c043d
Your Name <yacvals@gmail.com>
2024-05-29 14:28:37 +0300

    add graphic ui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
-package org.example;
+package org.example.ui;
+
+import org.example.logic.Command;
+import org.example.logic.CommandFactory;
+import org.example.storage.FileStorageProcessor;
+import org.example.storage.StorageProcessor;
+import org.example.storage.User;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserInterface ui = new GraphicUI();
+        StorageProcessor fileStorageProcessor = new FileStorageProcessor(ui);
+        try {
+            User user = fileStorageProcessor.getUserInfo();
+            CommandFactory commandFactory = new CommandFactory(ui, user);
+            do {
+                int choice = ui.showSuggestions();
+                Command command = commandFactory.createCommand(choice);
+                command.execute();
+            } while (ui.askToStop() == 0);
+            saveUser(fileStorageProcessor);
+            ui.sayBye();
+            Console.scan.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void saveUser(StorageProcessor storageProcessor) {
+        try {
+            storageProcessor.saveUserInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
